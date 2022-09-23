45bcbf94624c17fd40a740e227281fdf9a7c3f36
jaovw <joojvw@gmail.com>
2022-09-23 19:15:13 -0300

    routing

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+import router from './src/routes'
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+app.use(router)
+
+app.listen(port, () => console.info(`Rodando na porta:${port}`))
