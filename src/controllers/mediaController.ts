33941e109652736a543d0ec4e90f7f3329f45101
jaovw <joojvw@gmail.com>
2022-09-23 21:12:41 -0300

    endpoint media

--- src/controllers/mediaController.ts
+++ src/controllers/mediaController.ts
@@ -1,9 +1,27 @@
 import { Request, Response } from 'express'
 
-export function realizaMedia(req: Request, res: Response) {
+function media(a: number, b: number): number{
+
+    return Math.round((a + b)/2)
+}
+
+function resultadoMedia(req: Request, res: Response) {
+
+    const { a, b } = req.body
+
+    if(!a || !b){
+        return res.status(400).json({message: 'Favor utililizar o body corretamente ... '})
+    }
+
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        return res.status(400).json({message: 'Favor inserir somente numeros ...'})
+    }
+
+    const resultado: number = media(a,b)
 
     res.json({
-        message:'Hello World com controller'
+        resultado: resultado
     })
-
 }
+
+export { resultadoMedia }
