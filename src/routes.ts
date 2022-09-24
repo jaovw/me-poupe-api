ecdb7991c41af51b683dd3939a383bfc21cd9d1b
jaovw <joojvw@gmail.com>
2022-09-24 15:32:24 -0300

    rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { resultadoMedia } from './controllers/mediaController'
+import { getCep } from './controllers/cepController'
 
 const router = Router()
 
 router.get('/', resultadoMedia)
+router.get('/:cep', getCep)
 
 export default router
