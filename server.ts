8363a76bbebc1523ac0f00ca61bbae3683676fde
jaovw <joojvw@gmail.com>
2022-09-26 17:46:10 -0300

    implementando o logger

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express'
 import router from './src/routes'
+import logger from './src/utils/logger'
 
 const port = 5000
 
@@ -8,6 +9,6 @@ const app: Application = express()
 app.use(express.json())
 app.use(router)
 
-const server = app.listen(port, (): void => console.info(`Rodando na porta:${port}`))
+const server = app.listen(port, (): void => logger.info(`Rodando na porta:${port}`))
 
 export { app, server }
--- src/controllers/mediaController.ts
+++ src/controllers/mediaController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express'
+import logger from '../utils/logger'
 
 function media(a: number, b: number): number{
 
+    logger.info({params: {a,b}}, 'Parametros recebidos para a funcao media()')
     return Math.round((a + b)/2)
 }
 
@@ -10,10 +12,12 @@ function resultadoMedia(req: Request, res: Response) {
     const { a, b } = req.body
 
     if(!a || !b){
+        logger.error({params: {a,b}},'Favor utilizar o body corretamente ...')
         return res.status(400).json({message: 'Favor utilizar o body corretamente ...'})
     }
 
     if(typeof a !== 'number' || typeof b !== 'number'){
+        logger.error({params: {a,b}},'Favor inserir somente numeros ...')
         return res.status(400).json({message: 'Favor inserir somente numeros ...'})
     }
 
@@ -22,6 +26,7 @@ function resultadoMedia(req: Request, res: Response) {
     res.status(200).json({
         resultado: resultado
     })
+    logger.info({resultado: resultado}, 'Resultado da funcao media()')
 }
 
 export { resultadoMedia, media }
